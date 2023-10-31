d6e948b681dca500294098515d4c221af3b493f2
Sergei Nazarenko <nazarenkosi@36.sfr.gov.ru>
2023-10-31 16:32:21 +0300

    fix shellcheck issues

--- sphp.sh
+++ sphp.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 # created by: Sergei Nazarenko <nazares@icloud.com>
 
-# CLI colors
-RED='\e[0;31m'
-GREEN='\e[0;32m'
-YELLOW='\e[0;33m'
-NC='\e[0m'
-
 get_ostype () {
 	awk -F= '$1=="ID_LIKE" { print $2 ;}' /etc/os-release
 }
@@ -15,57 +9,57 @@ get_ostype () {
 os_type=$(get_ostype)
 
 if [[ ! $os_type =~ "debian" ]]; then
-	echo -e "${RED}Unsupported OS. Only debian-based systems are allowed${NC}"
+	printf '\e[0;31mUnsupported OS. Only debian-based systems are allowed\e[0m\n'
 	exit 1
 fi
 
 # get installed php versions
-versions=(`update-alternatives --list php | grep -oP "(\d\.\d+?)"`)
+
+mapfile -t versions < <(update-alternatives --list php | grep -oP "(\d\.\d+?)")
 
 if [ ! $# -eq 1 ]; then
-	printf "usage: sphp version\n"
+	printf 'usage: sphp version\n'
 	printf -v joined '%s,' "${versions[@]}"
-	printf "\n${YELLOW}installed php versions: %s${NC}\n\n" "${joined%,}"
+	printf '\n\e[0;33minstalled php versions: %s\e[0m\n\n' "${joined%,}"
 	exit 1
 else
 	if [[ " $1 " =~ [0-9]\.[0-9] ]]; then
-
 		switch_to=$1
 	else
-		printf "${RED}invalid version number.${NC}\n"
+		printf '\e[0;31minvalid version number.\e[0m\n'
 		exit 1
 	fi
 fi
 
-if [[ ! " ${versions[@]} " =~ " ${switch_to} " ]]; then
-	printf "${RED}version ${switch_to} is not installed.${NC}\n"
+if [[ !  ${versions[*]} =~  $switch_to  ]]; then
+	printf "\e[0;31mPHP version %s is not installed.\e[0m\n" "$switch_to"
 	exit 1
 fi
 
-printf "\n${GREEN}Switching to PHP ${switch_to}"
+printf '\n\e[0;32mSwitching to PHP %s' "$switch_to"
 
 switch_from=`php -v | grep -oP "PHP (\d\.\d+?)" | cut -d " " -f2`
 
-printf " from PHP ${switch_from}${NC}\n"
+printf ' from PHP %s\e[0m\n' "$switch_from"
 
-if [[ $switch_from == $switch_to ]]; then
-	printf "PHP ${switch_to} has been already set\n"
+if [ "$switch_from" == "$switch_to" ]; then
+	printf 'PHP %s has been already set\n' "$switch_to"
 	exit 1;
 fi
 
-printf "${YELLOW}this will required \`sudo\` privileges${NC}\n\n"
-printf "${GREEN}switching CLI${NC}\n"
+printf '\e[0;33mthis will required %s privileges\e[0m\n\n' "\`sudo\`"
+printf '\e[0;32mSwitching CLI\e[0m\n'
 
-sudo update-alternatives --set php /usr/bin/php$switch_to &&
-sudo update-alternatives --set phar /usr/bin/phar$switch_to &&
-sudo update-alternatives --set phar.phar /usr/bin/phar.phar$switch_to
+sudo update-alternatives --set php /usr/bin/php"$switch_to" &&
+sudo update-alternatives --set phar /usr/bin/phar"$switch_to" &&
+sudo update-alternatives --set phar.phar /usr/bin/phar.phar"$switch_to"
 
-printf "${GREEN}switching Apache${NC}\n"
+printf "\e[0;32mSwitching Apache\e[0m\n"
 
-sudo a2dismod php$switch_from
-sudo a2enmod php$switch_to
+sudo a2dismod php"$switch_from"
+sudo a2enmod php"$switch_to"
 
-read -p $'\e[0;32mActivate new config \'sudo systemctl restart apache2\' (y/N)?: \e[0m' yn
+read -r -p $'\e[0;32mActivate new config \'sudo systemctl restart apache2\' (y/N)?: \e[0m' yn
 
 case $yn in
 	[Yy]* ) sudo systemctl restart apache2;;
@@ -73,6 +67,6 @@ case $yn in
 	*) exit;;
 esac
 
-printf "\n${GREEN}php switch to $switch_to is now complete!${NC}\n\n"
+printf '\n\e[0;32mphp switch to PHP %s is now complete!\e[0m\n\n' "$switch_to"
 
 php -v
